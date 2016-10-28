c7150672d0c05653facfc086c899a889b4c1bcb9
DvdKhl <DvdKhl@gmail.com>
2016-10-28 03:43:11 +0200

    More user friendly error message for invalid arguments

--- AVDump3CL/Program.cs
+++ AVDump3CL/Program.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 namespace AVDump3CL {
 
 
-    class Program {
+	class Program {
 		private static CLSettingsHandler clSettingsHandler;
 
 		static void Main(string[] args) {
@@ -23,8 +23,8 @@ namespace AVDump3CL {
 					"--Conc=6",
 					"--BSize=8:8",
 					//"--Consumers=CRC32, ED2K, MD4, MD5, SHA1, SHA384, SHA512, TTH, TIGER, MKV",
-                    "--Consumers=TIGER-NATIVE",
-					//"--Consumers=SHA1,CRC32-NATIVE,TIGER-NATIVE",
+                    //"--Consumers",
+					"--Consumers=SHA1",
 					"--PrintHashes",
                     //"--Consumers=MKV",
                     //"--Reports=AVD3Report",
@@ -39,21 +39,37 @@ namespace AVDump3CL {
 					"--IncludePersonalData",
 					"--ErrorDirectory=Error",
 					"--PauseBeforeExit",
-                    "--NullStreamTest=100:1000000:1",
-                    //@"D:\MyStuff\BigFile",
-					//@"D:\MyStuff\SmallFile"
+                    //"--NullStreamTest=100:1000000:1",
+                    //@"D:\MyStuff\BigFile", //C548A93C
+                    @"D:\MyStuff\MediumFile", //5B64C2B0
+					//@"C:\Users\Arokh\AppData\Roaming\mIRC\downloads\"
 				};
 
-				File.WriteAllBytes(@"D:\MyStuff\SmallFile", Enumerable.Range(0, (1 << 20) + 2).Select(x => (byte)1).ToArray());
+				//if(true) {
+				//	using(var stream = File.Create(@"D:\MyStuff\MediumFile")) {
+				//		var b = new byte[1L << 30];
+				//		stream.Write(b, 0, b.Length);
+				//		stream.Write(b, 0, b.Length);
+				//	}
+				//}
+
+
+				//File.WriteAllBytes(@"D:\MyStuff\SmallFile", Enumerable.Range(0, (1 << 20) + 2).Select(x => (byte)1).ToArray());
 			}
+
 			clSettingsHandler = new CLSettingsHandler();
 
 			var moduleManagemant = IniModules();
 			moduleManagemant.RaiseBeforeConfiguration();
 
 			var pathsToProcess = new List<string>();
-			if(!clSettingsHandler.ParseArgs(args, pathsToProcess)) {
-				Console.Read();
+			try {
+				if(!clSettingsHandler.ParseArgs(args, pathsToProcess)) {
+					Console.Read();
+					return;
+				}
+			} catch(InvalidOperationException ex) {
+				Console.Error.WriteLine(ex.Message);
 				return;
 			}
 
