063987b191ef8051dd2eaba5828870b556d33703
DvdKhl <DvdKhl@gmail.com>
2016-10-29 22:55:59 +0200

    Description Color in Help not supported on Mono (fixes 1) Falling back to default

---
+++ AVDump3Lib/Misc/Utils.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AVDump3Lib.Misc {
+	public static class Utils {
+		public static bool UsingMono { get; } = Type.GetType("Mono.Runtime") != null;
+	}
+}
