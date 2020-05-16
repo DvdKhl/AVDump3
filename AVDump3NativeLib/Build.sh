4b4e671db78d42e9b1fa93622b0907f02ab59cde
David Kohl <dvdkhl+git@pm.me>
2020-05-16 12:18:26 +0200

    Moved to Makefile to build AVDump3NativeLib

--- AVDump3NativeLib/Build.sh
+++
@@ -1,1 +0,0 @@
-gcc -c -Wall -Werror -fpic -mavx -msha -msse4 -O3 /mnt/d/Projects/C#/AVDump3/AVDump3NativeLib/*.c -lrt; gcc -mavx -msha -msse4 -O3 -shared -o AVDump3NativeLib.so /mnt/d/Projects/C#/AVDump3/AVDump3NativeLib/*.o -lrt
