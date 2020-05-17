eae1640400e84c35564a992b653f1b2afa0fc0bf
David Kohl <dvdkhl+git@pm.me>
2020-05-17 17:22:22 +0200

    Docker experiments

--- .docker/Dockerfile-Entrypoint.sh
+++ .docker/Dockerfile-Entrypoint.sh
@@ -3,15 +3,23 @@
 doReset=0
 useVersion="-1"
 
-if [ "$1" == "RESET" ] || [ ! -f "/AVD3/AVDump3CL.dll" ]
+echo "Arguments: $@"
+
+if [ "$1" == "RESET" ]
 then
     doReset=1; shift
+fi
+
+if [ ! -f "/AVD3/AVDump3CL.dll" ]
+then
+    doReset=1;
 fi
 
 if [ "$1" == "USEVERSION" ] 
 then
     doReset=1; shift
-    useVersion=$2; shift
+    useVersion="$1"; shift
+    echo "Using Version $useVersion"
 fi
 
 if [ $doReset -eq 1 ] 
@@ -22,11 +30,13 @@ then
         useVersion=$(curl -s https://github.com/DvdKhl/AVDump3/releases/latest | sed -n -r 's/^.*DvdKhl\/AVDump3\/releases\/tag\/(B[[:digit:]]+).*$/\1/p')
 	echo "Latest Version: $useVersion"
     fi
-    
     rm -rf /AVD3/*
-    echo "Downloading Version: $useVersion"
-    curl -s -L --output /AVD3/AVDump3CL.zip "https://github.com/DvdKhl/AVDump3/releases/download/$useVersion/AVDump3CL-$useVersion.zip"
-    unzip -q /AVD3/AVDump3CL.zip
+    
+    dlUrl="https://github.com/DvdKhl/AVDump3/releases/download/$useVersion/AVDump3CL-$useVersion.zip"
+    echo "Downloading Version: $useVersion - $dlUrl"
+    curl -s -L --output /AVD3/AVDump3CL.zip "$dlUrl"
+    unzip -q /AVD3/AVDump3CL.zip -d /AVD3
+    ls -lh
 fi
 
 if [ "$1" != "NOOP" ]
