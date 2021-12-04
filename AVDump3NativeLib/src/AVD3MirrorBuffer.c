a2c295e99d18ca5bd670d5dbbe9e459dde1d258e
Oleg Oshmyan <chortos@inbox.lv>
2021-12-04 19:26:47 +0200

    AVDump3NativeLib: default to UNIX code even without __unix__
    For example, macOS is UNIX, but its compiler doesn't define __unix__.

--- AVDump3NativeLib/src/AVD3MirrorBuffer.c
+++ AVDump3NativeLib/src/AVD3MirrorBuffer.c
@@ -1,64 +1,6 @@
 #define _GNU_SOURCE
 #include "AVD3NativeLibApi.h"
 
-#ifdef __unix__
-
-#include <stdio.h>
-#include <assert.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <errno.h>
-
-char* CreateMirrorBuffer(uint32_t minLength, AVD3MirrorBufferCreateHandle* handle) {
-	uint32_t pageSize = getpagesize();
-
-	uint32_t pageCount = minLength / pageSize + (minLength % pageSize == 0 ? 0 : 1);
-	if (pageCount > UINT32_MAX / pageSize / 2) return "requested length is too big";
-	uint32_t length = pageCount * pageSize;
-
-	char path[] = "/AVD3WrapAroundBuffer";
-	int fd = shm_open(path, O_RDWR | O_CREAT | O_EXCL, 0600);
-	if (fd == -1) return "shm_open returned -1";
-	int success = shm_unlink(path);
-	if (success != 0) return "shm_unlink returned non-zero";
-
-	success = ftruncate(fd, length * 2);
-	if (success != 0) return "ftruncate returned non-zero";
-
-	uint8_t* data = mmap(0, length * 2, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-	if (data == (void*)-1)return "mmap (first) returned -1";
-
-	uint8_t* mirror = mmap(data + length, length, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, fd, 0);
-	if (mirror == (void*)-1) {
-		if (munmap(data, length) != 0) return "munmap returned non-zero";
-		return "mmap (second) returned -1";
-	}
-
-	close(fd);
-
-	for (size_t i = 0; i < length; i++) {
-		data[i] = (uint8_t)i;
-		if (data[i] != mirror[i]) return "Data Mirroring failed";
-		data[i] = 0;
-	}
-
-	handle->fileHandle = 0;
-	handle->baseAddress = data;
-	handle->length = length;
-
-	return 0;
-}
-
-char* FreeMirrorBuffer(AVD3MirrorBufferCreateHandle * handle) {
-
-	return 0;
-}
-
-#endif
-
-
 #ifdef _WIN32
 
 #include <Windows.h>
@@ -114,6 +56,61 @@ char* FreeMirrorBuffer(AVD3MirrorBufferCreateHandle* handle) {
 	if (!UnmapViewOfFile(handle->baseAddress)) return "First UnmapViewOfFile failed";
 	if (!UnmapViewOfFile(handle->baseAddress + handle->length)) return "Second UnmapViewOfFile failed";
 	if (!CloseHandle(handle->fileHandle)) return "CloseHandle failed";
+	return 0;
+}
+
+#else
+
+#include <stdio.h>
+#include <assert.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+char* CreateMirrorBuffer(uint32_t minLength, AVD3MirrorBufferCreateHandle* handle) {
+	uint32_t pageSize = getpagesize();
+
+	uint32_t pageCount = minLength / pageSize + (minLength % pageSize == 0 ? 0 : 1);
+	if (pageCount > UINT32_MAX / pageSize / 2) return "requested length is too big";
+	uint32_t length = pageCount * pageSize;
+
+	char path[] = "/AVD3WrapAroundBuffer";
+	int fd = shm_open(path, O_RDWR | O_CREAT | O_EXCL, 0600);
+	if (fd == -1) return "shm_open returned -1";
+	int success = shm_unlink(path);
+	if (success != 0) return "shm_unlink returned non-zero";
+
+	success = ftruncate(fd, length * 2);
+	if (success != 0) return "ftruncate returned non-zero";
+
+	uint8_t* data = mmap(0, length * 2, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	if (data == (void*)-1)return "mmap (first) returned -1";
+
+	uint8_t* mirror = mmap(data + length, length, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_FIXED, fd, 0);
+	if (mirror == (void*)-1) {
+		if (munmap(data, length) != 0) return "munmap returned non-zero";
+		return "mmap (second) returned -1";
+	}
+
+	close(fd);
+
+	for (size_t i = 0; i < length; i++) {
+		data[i] = (uint8_t)i;
+		if (data[i] != mirror[i]) return "Data Mirroring failed";
+		data[i] = 0;
+	}
+
+	handle->fileHandle = 0;
+	handle->baseAddress = data;
+	handle->length = length;
+
+	return 0;
+}
+
+char* FreeMirrorBuffer(AVD3MirrorBufferCreateHandle * handle) {
+
 	return 0;
 }
 
